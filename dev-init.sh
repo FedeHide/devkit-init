4615e37805897ea421588cf068a456233951243a
larcdian <larcdian@gmail.com>
2024-02-08 08:24:22 -0300

    refactor: now create directories and run build dependencies from this script

--- dev-init.sh
+++ dev-init.sh
@@ -8,14 +8,31 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo "🦁 creating directories 🦁"
+mkdir -p $outputDirectory/dist/js $outputDirectory/dist/css $outputDirectory/src/scss/base $outputDirectory/src/scss/components $outputDirectory/src/scss/layout $outputDirectory/src/scss/utils $outputDirectory/src/ts $outputDirectory/public/favicon
+
 node init.js "$outputDirectory"
 
 # Waiting for init.js to finish executing
 read -p "Press Enter to continue..."
 
-(cd "$outputDirectory" && pnpm run template-init)
+echo "🦁 installing dependencies 🦁"
+cd "$outputDirectory"
+pnpm install -D sass
+pnpm install -D node-sass
+pnpm install -D nodemon
+pnpm install -D prettier
+tsc --init
+pnpx eslint --init
+pnpm install -D eslint-config-prettier
+touch CONTRIBUTING.md src/ts/main.ts
+node update-tsconfig.mjs
+rm update-tsconfig.mjs
+git init
+git add .
+git commit -m 'initial commit'
 
 # open Visual Studio Code
 code "$outputDirectory"
 
-echo "Process completed successfully 🦁"
+echo "🦁 Process completed successfully 🦁"
