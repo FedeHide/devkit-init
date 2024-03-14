719eb7bc0f51f0921d325547c5cd47fe2b68e5f5
FedeHide <larcdian@gmail.com>
2024-03-14 15:23:04 -0300

    feat: progress bar implementation and refactor

--- devkit-init.sh
+++ devkit-init.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
+## VARIABLES
+# COLOR VARIABLES
 GREY="\033[1;30m"
 RED="\033[0;31m"
 LIGHT_BLUE='\x1b[38;5;14m'
 DARK_BLUE='\x1b[38;5;63m'
 BLUE='\x1b[38;5;105m'
+DARK_YELLOW='\x1b[38;5;178m'
 PINK='\033[0;35m'
 RESET_COLOR="\033[0m"
+
 CLEAR_LINE="\033[1A\033[2K"
 
-# Obtiene la ruta al directorio actual
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# PROGRESS BAR VARIABLES
+GREEN='\e[0;32m'
+RESET_BLOCK='\e[0m'
+duration=10
+step=1
+progress=0
+line=""
 
+## PROGRESS BAR FUNCTION
+progress_bar() {
+    
+    # terminal width
+    local terminal_width=$(($(tput cols) / 2))
+
+    # initiar bar width
+    local full_bar
+    full_bar=$(printf '=%.0s' $(seq 1 $((terminal_width * 8 / 10))))
+
+    # calculate bar length
+    local progress_length=$(( (progress * terminal_width * 8 / 10) / duration ))
+
+    # print progress bar
+    line=$(printf "%-${progress_length}s" " ")
+    line=${line// /█}
+    echo -ne "\r[${GREEN}${line}${RESET_BLOCK}${full_bar:${progress_length}}] $(( (progress * 100) / duration ))%"
+    ((progress += step))
+}
+
+## MAIN LOGIC
+# get temporal package npx directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKAGE_DIR="$(dirname "$DIR")/devkit-init"
 
 # handle user cancel with SIGINT (Ctrl + C)
@@ -20,7 +52,6 @@ sigint_handler() {
     echo -e "${CLEAR_LINE}❌ Operation cancelled" >&2
     exit 1
 }
-# associate the sigint_handler() function with the SIGINT signal
 trap 'sigint_handler' SIGINT
 
 
@@ -35,35 +66,35 @@ get_project_dir() {
         project_dir="$input"
     fi
 }
-
 get_project_dir
 
 outputDirectory=$project_dir 
-
 echo -e "${CLEAR_LINE}✔️ Creating project: $outputDirectory"
+echo "🦁 Select Technologies:"
 
 # select techs
 is_tech() {
-
     local tech="$1"
-
-    echo -ne "${RED}?${RESET_COLOR} Do you want to use ${tech}? ${GREY}yes / no${RESET_COLOR} "
+    echo -ne "${RED}?${RESET_COLOR} ${tech}? ${GREY}yes / no${RESET_COLOR} "
     read -rp "" choice
-
     choice_lowercase=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
 
     # check the user's choice
     case $choice_lowercase in
         y|yes|"") 
-            echo -e "${CLEAR_LINE}✔️ Do you want to use ${tech}?"
             tech_choice=true
+            echo -e "${CLEAR_LINE}✔️  ${tech}"
             ;;
         n|no)
-            echo -e "${CLEAR_LINE}❌ Do you want to use ${tech}?"
-            tech_choice=false
+            if [ "${tech}" = "${LIGHT_BLUE}React${RESET_COLOR}" ]; then
+                echo -e "${CLEAR_LINE}✔️  ${DARK_YELLOW}Javascript${RESET_COLOR}"
+            else
+                echo -e "${CLEAR_LINE}❌ ${tech}"
+                tech_choice=false
+            fi
             ;;
         *) 
-            echo -e "${CLEAR_LINE}❌ Do you want to use ${tech}?"
+            echo -e "${CLEAR_LINE}❌ ${tech}"
             echo -e "❌ Operation cancelled -> Invalid option"
             exit 0
             ;;
@@ -83,13 +114,15 @@ if [ "$is_react" = true ]; then
     is_tech "${BLUE}Tailwind${RESET_COLOR}"
     is_tailwind=$tech_choice
 fi
-
 # Sass choice
 is_tech "${PINK}Sass${RESET_COLOR}"
 is_sass=$tech_choice
 
+progress_bar
 mkdir "$outputDirectory" && cd "$outputDirectory" || exit 1
 
+
+# FOLDERS
 dist_folders=(
     "dist/js"
     "dist/css"
@@ -115,8 +148,7 @@ root_folders=(
     "public/images"
 )
 
-echo "🦁 Installing Dependencies 🦁"
-
+progress_bar
 # REACT
 if [ "$is_react" = true ]; then
     if [ "$is_typescript" = true ]; then
@@ -132,8 +164,9 @@ if [ "$is_react" = true ]; then
     fi
     cd ..
     pnpm create next-app@latest "$outputDirectory" ${next_ts_flag} ${next_tw_flag} --no-eslint --app --src-dir --import-alias default >/dev/null 2>&1
+    progress_bar
     rm "$outputDirectory/next.config.mjs"
-    rm -rf .git
+    rm -rf "$outputDirectory/.git"
     cd "$outputDirectory" || exit 1
 else
     for folder in "${dist_folders[@]}" "${root_folders[@]}"; do
@@ -141,6 +174,7 @@ else
     done
     pnpm init > /dev/null 2>&1
     pnpm install -D rollup >/dev/null 2>&1
+    progress_bar
     touch rollup.config.js
 fi
 
@@ -149,6 +183,7 @@ if [[ "$is_typescript" = true && "$is_react" = false ]]; then
     for folder in "${ts_folders[@]}"; do
         mkdir -p "$folder"
         touch src/ts/main.ts
+        progress_bar
     done
 fi
 
@@ -156,6 +191,7 @@ if [[ "$is_typescript" = false && "$is_react" = false ]]; then
     for folder in "${js_folders[@]}"; do
         mkdir -p "$folder"
         touch src/js/main.js
+        progress_bar
     done
 fi
 
@@ -168,26 +204,32 @@ fi
 for folder in "${sass_folders[@]}"; do
     mkdir -p "$folder"
 done
+progress_bar
 
-
+# PRETTIER & ESLINT RULES
 pnpm install -D prettier >/dev/null 2>&1
 if [ "$is_typescript" = true ]; then
     pnpm install -D typescript@latest eslint-config-standard-with-typescript@latest >/dev/null 2>&1
+    progress_bar
 else
     pnpm install -D eslint-config-standard@latest >/dev/null 2>&1
+    progress_bar
 fi
+
 pnpm install -D eslint-plugin-react@latest @typescript-eslint/eslint-plugin@^6.4.0 eslint@latest eslint-plugin-import@latest eslint-plugin-n@latest eslint-plugin-promise@latest >/dev/null 2>&1
+progress_bar
 pnpm install -D eslint-plugin-prettier@latest >/dev/null 2>&1
 pnpm install -D eslint-config-prettier >/dev/null 2>&1
+progress_bar
 touch .eslintrc.json
 
 if [ "$is_react" = true ]; then
     pnpm install -D eslint-config-next@latest >/dev/null 2>&1
 fi
 
+# TEMPLATES
 cd ..
-
-echo "🦁 Updating files 🦁"
+progress_bar
 # VANILLA template || REACT template
 if [ "$is_react" = false ]; then
     node "$PACKAGE_DIR/scripts/vanillaInit.js" "$outputDirectory"
@@ -202,6 +244,7 @@ fi
 if [[ "$is_typescript" = true && "$is_react" = false ]]; then
     node "$PACKAGE_DIR/scripts/typescriptInit.js" "$outputDirectory"
 fi
+progress_bar
 
 # CLEANING
 if [ "$is_react" = true ]; then
@@ -213,14 +256,15 @@ if [ "$is_sass" = false ]; then
     mv "$outputDirectory"/src/app/globals.scss "$outputDirectory"/src/app/globals.css
     sed -i 's/globals\.scss/globals\.css/' "$outputDirectory"/src/app/layout.jsx
 fi
-
-cd "$outputDirectory" || exit 1
+progress_bar
 
 # Initial commit
+cd "$outputDirectory" || exit 1
 git init >/dev/null 2>&1
 git add . >/dev/null 2>&1
 git commit -m 'initial commit' >/dev/null 2>&1
-echo "🦁 Process completed successfully 🦁"
+progress_bar
+echo -ne "${CLEAR_LINE}🦁 Process completed successfully 🦁"
 
 # open Visual Studio Code
 cd ..
